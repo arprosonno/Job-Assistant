bb2b81a08f59185a02927be7319afc46808ef289
AR Prosonno <issacprosonno@gmail.com>
2025-07-19 00:59:41 +0600

    Update server.js

--- server.js
+++ server.js
@@ -79,6 +79,7 @@ app.delete('/jobs/:id', async (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-  console.log('Server running at http://localhost:5000');
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
