168f1aec13dc82aeeef676f58d0a1cdd4aa24438
AR Prosonno <issacprosonno@gmail.com>
2025-07-19 01:50:58 +0600

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/jobportal', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
 
 // User Schema
 const userSchema = new mongoose.Schema({
